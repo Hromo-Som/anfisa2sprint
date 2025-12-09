7edd799daf2a8c4a9ec38f1240722ebb8de67cf2
Темур Хайдаров <temur.haydarov@gmail.com>
2025-12-09 15:51:47 +0500

    Изучение Django ORM

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    ice_cream_list = IceCream.objects.values(
+        'title', 'id', 'description', 'price'
+        ).filter(is_published=True,
+                 is_on_main=True,
+                 category__is_published=True
+                 ).order_by('output_order', 'title')[0:3]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Category, Topping, Wrapper, IceCream
 
-# Register your models here.
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamInline(admin.StackedInline):
+
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "description",
+        "is_published",
+        "is_on_main",
+        "category",
+        "wrapper",
+        "output_order",
+    )
+    list_editable = ("is_published", "is_on_main", "category", "output_order")
+    search_fields = ("title",)
+    list_filter = ("category",)
+    list_display_links = ("title",)
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
